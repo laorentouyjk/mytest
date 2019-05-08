7dce2f006cd9d2eed387fbdf90fb60e7cb5ed454
laorentouyjk <yjk_java@aliyun.com>
2019-05-08 17:40:14 +0800

    swagger 配置提交

--- src/main/java/com/yijiankang/RedisdemoApplication.java
+++ src/main/java/com/yijiankang/RedisdemoApplication.java
@@ -1,10 +1,11 @@
-package com.yijiankang.redisdemo;
+package com.yijiankang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisdemoApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(RedisdemoApplication.class, args);
--- src/main/java/com/yijiankang/redisdemo/controller/RedisController.java
+++ src/main/java/com/yijiankang/redisdemo/controller/RedisController.java
@@ -1,8 +1,22 @@
 package com.yijiankang.redisdemo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "测试redis")
 @RestController
+@RequestMapping("api")
 public class RedisController {
+
+    @ApiOperation(value="测试redis应用")
+    @PostMapping(value="/testRedis")
+
+    public void  TestRedis(){
+        System.out.println("1111");
+
+    }
 
 }
