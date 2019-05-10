7783b8bffd60ee11694977184c2a32921280f7a2
yjk <yjk_java@aliyun.com>
2019-05-10 08:00:27 +0800

    topic  websocket

---
+++ src/main/java/com/yijiankang/websocketdemo/config/WebSocketTopic.java
@@ -0,0 +1,6 @@
+package com.yijiankang.websocketdemo.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketTopic implements WebSocketMessageBrokerConfigurer {
+}
