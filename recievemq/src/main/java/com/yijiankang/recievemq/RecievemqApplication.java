366f536c62ec5a920df581de880b13e9604d2939
laorentouyjk <yjk_java@aliyun.com>
2019-05-22 17:53:55 +0800

    rabbitmqdemo

---
+++ recievemq/src/main/java/com/yijiankang/recievemq/RecievemqApplication.java
@@ -0,0 +1,13 @@
+package com.yijiankang.recievemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecievemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecievemqApplication.class, args);
+    }
+
+}
