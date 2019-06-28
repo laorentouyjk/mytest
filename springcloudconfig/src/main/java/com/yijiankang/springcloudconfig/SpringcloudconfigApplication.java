6225985f79a2224b97a01c7910f1a6587c6e0ad1
laorentouyjk <yjk_java@aliyun.com>
2019-06-28 18:23:43 +0800

    springcloud config p

--- springcloudconfig/src/main/java/com/yijiankang/springcloudconfig/SpringcloudconfigApplication.java
+++ springcloudconfig/src/main/java/com/yijiankang/springcloudconfig/SpringcloudconfigApplication.java
@@ -2,10 +2,12 @@ package com.yijiankang.springcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SpringcloudconfigApplication {
 
     public static void main(String[] args) {
--- springcloudconfigclient/src/main/java/com/yijiankang/springcloudconfigclient/SpringcloudconfigclientApplication.java
+++ springcloudconfigclient/src/main/java/com/yijiankang/springcloudconfigclient/SpringcloudconfigclientApplication.java
@@ -2,8 +2,10 @@ package com.yijiankang.springcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringcloudconfigclientApplication {
 
     public static void main(String[] args) {
